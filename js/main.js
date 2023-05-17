3740be27b8ad1fa44a3f774ac841424f83c478a3
Yessin <34749@ma-web.nl>
2023-05-17 14:36:10 +0200

    nieuwe update

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ button.onclick = function() {
     navigation.style.opacity = 1;
     body.style.overflow = "hidden";
 }
+
+let shoppingcart = {
+    "price": 0.0,
+    "items": {}
+}
+function add_shoppingcart(name, item) {
+    // add new item
+    shoppingcart["items"][name] = item;
+    // update price
+    shoppingcart["price"] += item["price"];
+}
+
+function del_shoppingcart(name) {
+    if (name in shoppingcart.items) {
+        // update price
+        shoppingcart["price"] -= shoppingcart["items"][name]["price"];
+        // remove item
+        delete shoppingcart[name];
+    }
+}
